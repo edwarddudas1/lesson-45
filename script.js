6ceb5cb653bd93ef7d9f4becf5dcdcf46750478c
Edward Dudas <edwarddudas@gmail.com>
2024-06-11 18:22:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const openModal = document.querySelector(".button");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector("modal .button");
+console.log(modal)
+console.log(closeModal);
